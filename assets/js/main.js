14ee7342d8dcd7d6427ae0e64634c31699fec7b1
NicoleBorbe <nicole.borbe@gmail.com>
2022-11-14 23:27:28 +0100

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function fahrrad() {
+    console.log(this.name);
+}
+
+let name = "Canyon";
+let obj1 = { name: "Merida", fahrrad: fahrrad };
+let obj2 = { name: "Scott", fahrrad: fahrrad };
+
+fahrrad();
+// leere Zeile
+
+obj1.fahrrad();
+// Merida
+
+obj2.fahrrad();
+// Scott
